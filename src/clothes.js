b2d4b0e2855b1d2f8a69c09eed27dba05887af8b
Emox133 <emir.betaacc0unt@gmail.com>
2019-12-26 19:27:59 +0100

    Essential workflow setup

---
+++ src/clothes.js
@@ -0,0 +1,24 @@
+let items = [
+    {
+        sys: {
+            id: 1
+        },
+        
+        fields: {
+            title: 'Black t-shirt',
+            price: 20,
+            material: 'cotton',
+            image: {
+                fields: {
+                    file: {
+                        url: './assets/images/black-t-shirt.jpg'
+                    }
+                }
+            },
+            count: 1,
+            manufacturer: 'balenciaga',
+            total: 20,
+            category: 't-shirts'
+        }
+    }
+];
