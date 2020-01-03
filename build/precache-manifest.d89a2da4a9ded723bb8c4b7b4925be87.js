05a4c864748e6b89d6c5bf370e8a9286bfff9c76
Emox133 <emir.betaacc0unt@gmail.com>
2020-01-03 12:58:09 +0100

    Adding redirect file manualy

---
+++ build/precache-manifest.d89a2da4a9ded723bb8c4b7b4925be87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b435c43036a6907921f890165895e76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8438f597f6dff135b8a",
+    "url": "/static/css/main.c41bbe43.chunk.css"
+  },
+  {
+    "revision": "c5c632b0d89d108dc6dd",
+    "url": "/static/js/2.1d537256.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.1d537256.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8438f597f6dff135b8a",
+    "url": "/static/js/main.b841462b.chunk.js"
+  },
+  {
+    "revision": "9888bb8026e3523d3bf4",
+    "url": "/static/js/runtime-main.2a39f7cd.js"
+  },
+  {
+    "revision": "d4aae8a64003fd686e329e9047b87aef",
+    "url": "/static/media/logo.d4aae8a6.png"
+  }
+]);
