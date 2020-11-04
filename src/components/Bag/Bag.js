c7ac929868a897918e08ce7a4664a641c1ed6dfe
Emox133 <emir.betaaacc0unt@gmail.com>
2020-11-04 10:17:10 +0100

    heroku redeploy

--- src/components/Bag/Bag.js
+++ src/components/Bag/Bag.js
@@ -6,7 +6,6 @@ import BagEmpty from './BagEmpty'
 // ALT WAY OF GETTING BAG ITEMS
 // let bagItems;
 // {value.bag ? bagItems = value.bag : bagItems = []}
-// console.log(bagItems)
 
 const Bag = (props) => {
     return (
