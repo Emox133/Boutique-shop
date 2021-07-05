f1cd2caa0d86fd40137dc0abf93d2c31c881d90b
Emox133 <emir.betaacc0unt@gmail.com>
2021-07-05 17:31:50 +0200

    refactor

--- src/clothes.js
+++ src/clothes.js
@@ -22,172 +22,333 @@ export const items = [
             inBag: false
         }
     },
-
     {
         sys: {
-            id: 2
+            id: 1
         },
         
         fields: {
-            title: 'Black coat',
-            price: 40,
-            material: 'elastin',
+            title: 'Crna majica',
+            price: 20,
+            material: 'cotton',
             image: {
                 fields: {
                     file: {
-                        url: 'images/black-coat.jpg'
-                    }
-                }
-            },
-            count: 1,
-            manufacturer: 'nike',
-            total: 40,
-            category: 'coats',
-            inBag: false
-        }
-    },
-
-    {
-        sys: {
-            id: 3
-        },
-        
-        fields: {
-            title: 'Black shoes',
-            price: 30,
-            material: 'leather',
-            image: {
-                fields: {
-                    file: {
-                        url: 'images/black-shoes.jpg'
-                    }
-                }
-            },
-            count: 1,
-            manufacturer: 'adidas',
-            total: 30,
-            category: 'shoes',
-            inBag: false
-        }
-    },
-
-    {
-        sys: {
-            id: 4
-        },
-        
-        fields: {
-            title: 'Blue shoes',
-            price: 25,
-            material: 'leather',
-            image: {
-                fields: {
-                    file: {
-                        url: 'images/blue-shoes.jpg'
-                    }
-                }
-            },
-            count: 1,
-            manufacturer: 'reebook',
-            total: 25,
-            category: 'shoes',
-            inBag: false
-        }
-    },
-
-    {
-        sys: {
-            id: 5
-        },
-        
-        fields: {
-            title: 'Brown coat',
-            price: 50,
-            material: 'elastin',
-            image: {
-                fields: {
-                    file: {
-                        url: 'images/brown-coat.jpg'
+                        url: 'images/black-t-shirt.jpg'
                     }
                 }
             },
             count: 1,
             manufacturer: 'balenciaga',
-            total: 50,
-            category: 'coats',
-            inBag: false
-        }
-    },
-
-    {
-        sys: {
-            id: 6
-        },
-        
-        fields: {
-            title: 'Brown hat',
-            price: 10,
-            material: 'cotton',
-            image: {
-                fields: {
-                    file: {
-                        url: 'images/brown-hat.jpg'
-                    }
-                }
-            },
-            count: 1,
-            manufacturer: 'gucci',
-            total: 10,
-            category: 'hats',
-            inBag: false
-        }
-    },
-
-    {
-        sys: {
-            id: 7
-        },
-        
-        fields: {
-            title: 'Crimson shirt',
-            price: 90,
-            material: 'elastin',
-            image: {
-                fields: {
-                    file: {
-                        url: 'images/crimson-shirt.jpg'
-                    }
-                }
-            },
-            count: 1,
-            manufacturer: 'Tommy hilfiger',
-            total: 90,
-            category: 'shirts',
-            inBag: false
-        }
-    },
-
-    {
-        sys: {
-            id: 8
-        },
-        
-        fields: {
-            title: 'White t-shirt',
-            price: 15,
-            material: 'cotton',
-            image: {
-                fields: {
-                    file: {
-                        url: 'images/white-t-shirt.jpg'
-                    }
-                }
-            },
-            count: 1,
-            manufacturer: 'reebook',
-            total: 15,
+            total: 20,
             category: 't-shirts',
             inBag: false
         }
     },
+    {
+        sys: {
+            id: 1
+        },
+        
+        fields: {
+            title: 'Crna majica',
+            price: 20,
+            material: 'cotton',
+            image: {
+                fields: {
+                    file: {
+                        url: 'images/black-t-shirt.jpg'
+                    }
+                }
+            },
+            count: 1,
+            manufacturer: 'balenciaga',
+            total: 20,
+            category: 't-shirts',
+            inBag: false
+        }
+    },
+    {
+        sys: {
+            id: 1
+        },
+        
+        fields: {
+            title: 'Crna majica',
+            price: 20,
+            material: 'cotton',
+            image: {
+                fields: {
+                    file: {
+                        url: 'images/black-t-shirt.jpg'
+                    }
+                }
+            },
+            count: 1,
+            manufacturer: 'balenciaga',
+            total: 20,
+            category: 't-shirts',
+            inBag: false
+        }
+    },
+    {
+        sys: {
+            id: 1
+        },
+        
+        fields: {
+            title: 'Crna majica',
+            price: 20,
+            material: 'cotton',
+            image: {
+                fields: {
+                    file: {
+                        url: 'images/black-t-shirt.jpg'
+                    }
+                }
+            },
+            count: 1,
+            manufacturer: 'balenciaga',
+            total: 20,
+            category: 't-shirts',
+            inBag: false
+        }
+    },
+    {
+        sys: {
+            id: 1
+        },
+        
+        fields: {
+            title: 'Crna majica',
+            price: 20,
+            material: 'cotton',
+            image: {
+                fields: {
+                    file: {
+                        url: 'images/black-t-shirt.jpg'
+                    }
+                }
+            },
+            count: 1,
+            manufacturer: 'balenciaga',
+            total: 20,
+            category: 't-shirts',
+            inBag: false
+        }
+    },
+    {
+        sys: {
+            id: 1
+        },
+        
+        fields: {
+            title: 'Crna majica',
+            price: 20,
+            material: 'cotton',
+            image: {
+                fields: {
+                    file: {
+                        url: 'images/black-t-shirt.jpg'
+                    }
+                }
+            },
+            count: 1,
+            manufacturer: 'balenciaga',
+            total: 20,
+            category: 't-shirts',
+            inBag: false
+        }
+    },
+    {
+        sys: {
+            id: 1
+        },
+        
+        fields: {
+            title: 'Crna majica',
+            price: 20,
+            material: 'cotton',
+            image: {
+                fields: {
+                    file: {
+                        url: 'images/black-t-shirt.jpg'
+                    }
+                }
+            },
+            count: 1,
+            manufacturer: 'balenciaga',
+            total: 20,
+            category: 't-shirts',
+            inBag: false
+        }
+    },
+
+    // {
+    //     sys: {
+    //         id: 2
+    //     },
+        
+    //     fields: {
+    //         title: 'Black coat',
+    //         price: 40,
+    //         material: 'elastin',
+    //         image: {
+    //             fields: {
+    //                 file: {
+    //                     url: 'images/black-coat.jpg'
+    //                 }
+    //             }
+    //         },
+    //         count: 1,
+    //         manufacturer: 'nike',
+    //         total: 40,
+    //         category: 'coats',
+    //         inBag: false
+    //     }
+    // },
+
+    // {
+    //     sys: {
+    //         id: 3
+    //     },
+        
+    //     fields: {
+    //         title: 'Black shoes',
+    //         price: 30,
+    //         material: 'leather',
+    //         image: {
+    //             fields: {
+    //                 file: {
+    //                     url: 'images/black-shoes.jpg'
+    //                 }
+    //             }
+    //         },
+    //         count: 1,
+    //         manufacturer: 'adidas',
+    //         total: 30,
+    //         category: 'shoes',
+    //         inBag: false
+    //     }
+    // },
+
+    // {
+    //     sys: {
+    //         id: 4
+    //     },
+        
+    //     fields: {
+    //         title: 'Blue shoes',
+    //         price: 25,
+    //         material: 'leather',
+    //         image: {
+    //             fields: {
+    //                 file: {
+    //                     url: 'images/blue-shoes.jpg'
+    //                 }
+    //             }
+    //         },
+    //         count: 1,
+    //         manufacturer: 'reebook',
+    //         total: 25,
+    //         category: 'shoes',
+    //         inBag: false
+    //     }
+    // },
+
+    // {
+    //     sys: {
+    //         id: 5
+    //     },
+        
+    //     fields: {
+    //         title: 'Brown coat',
+    //         price: 50,
+    //         material: 'elastin',
+    //         image: {
+    //             fields: {
+    //                 file: {
+    //                     url: 'images/brown-coat.jpg'
+    //                 }
+    //             }
+    //         },
+    //         count: 1,
+    //         manufacturer: 'balenciaga',
+    //         total: 50,
+    //         category: 'coats',
+    //         inBag: false
+    //     }
+    // },
+
+    // {
+    //     sys: {
+    //         id: 6
+    //     },
+        
+    //     fields: {
+    //         title: 'Brown hat',
+    //         price: 10,
+    //         material: 'cotton',
+    //         image: {
+    //             fields: {
+    //                 file: {
+    //                     url: 'images/brown-hat.jpg'
+    //                 }
+    //             }
+    //         },
+    //         count: 1,
+    //         manufacturer: 'gucci',
+    //         total: 10,
+    //         category: 'hats',
+    //         inBag: false
+    //     }
+    // },
+
+    // {
+    //     sys: {
+    //         id: 7
+    //     },
+        
+    //     fields: {
+    //         title: 'Crimson shirt',
+    //         price: 90,
+    //         material: 'elastin',
+    //         image: {
+    //             fields: {
+    //                 file: {
+    //                     url: 'images/crimson-shirt.jpg'
+    //                 }
+    //             }
+    //         },
+    //         count: 1,
+    //         manufacturer: 'Tommy hilfiger',
+    //         total: 90,
+    //         category: 'shirts',
+    //         inBag: false
+    //     }
+    // },
+
+    // {
+    //     sys: {
+    //         id: 8
+    //     },
+        
+    //     fields: {
+    //         title: 'White t-shirt',
+    //         price: 15,
+    //         material: 'cotton',
+    //         image: {
+    //             fields: {
+    //                 file: {
+    //                     url: 'images/white-t-shirt.jpg'
+    //                 }
+    //             }
+    //         },
+    //         count: 1,
+    //         manufacturer: 'reebook',
+    //         total: 15,
+    //         category: 't-shirts',
+    //         inBag: false
+    //     }
+    // },
 ]
